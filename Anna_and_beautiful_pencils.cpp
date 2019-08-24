d350cadfc6bc2e0d01fc2e92586c7fb23c295f8d
codingbeeast <43074149+codingbeeast@users.noreply.github.com>
2019-08-24 20:18:47 +0530

    Add files via upload

---
+++ Anna_and_beautiful_pencils.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int remove(string s){
+    int count=0;
+    for(int i=0;i<s.length();){
+        if(i==s.length()-1) return ++count;
+        else if(s[i]+1==s[i+1]){
+            count++;
+            i++;
+        }
+        else i+=2;
+    }
+    return count;
+}
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    cout<<remove(s)<<endl;
+    return 0;
+}
+
