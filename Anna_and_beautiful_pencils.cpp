5be99aaef0fd9fb04a501cb0de9a46a3bed9f191
codingbeeast <43074149+codingbeeast@users.noreply.github.com>
2019-08-27 05:25:02 +0530

    Update Anna_and_beautiful_pencils.cpp

--- Anna_and_beautiful_pencils.cpp
+++ Anna_and_beautiful_pencils.cpp
@@ -4,7 +4,7 @@ int remove(string s){
     int count=0;
     for(int i=0;i<s.length();){
         if(i==s.length()-1) return ++count;
-        else if(s[i]+1==s[i+1]){
+        else if(s[i]==s[i+1]){
             count++;
             i++;
         }
